20ac1b8205f53e7da91fcb73f0bbd1d216208a85
JINESHBHASKAR <jineshbhaskarkp@gmail.com>
2021-12-27 22:05:33 +0530

    new components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { UpdateCustomerComponent } from './update-customer/update-customer.component';
+import { CreateCustomerComponent } from './create-customer/create-customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    CustomerListComponent,
+    CustomerDetailsComponent,
+    UpdateCustomerComponent,
+    CreateCustomerComponent
   ],
   imports: [
     BrowserModule,
